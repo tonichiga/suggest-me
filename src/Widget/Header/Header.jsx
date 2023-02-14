09820d3095628735385f96ad47e91699d3a966fc
MastaBata <60700819+MastaBata@users.noreply.github.com>
2023-02-14 14:23:30 +0200

    new header

--- src/Widget/Header/Header.jsx
+++ src/Widget/Header/Header.jsx
@@ -1,7 +1,21 @@
 import styles from "./Header.module.scss";
-
+import logo from "../../assets/images/logo.png";
+import arrowRight from "../../assets/images/arrowRight.svg";
 const Header = () => {
-  return <header className={styles.header}>Header</header>;
+    return (
+       <header className={styles.header}>
+           <div className={styles.container}>
+               <img src={logo} alt="logo"/>
+               <nav>
+                   <li><a href="#">Movies</a></li>
+                   <li><a href="#">TV Shows</a></li>
+                   <li><a href="#">Suggest me</a>
+                       <img className={styles.arrowRight} src={arrowRight} alt="arrowRight"/>
+                   </li>
+               </nav>
+           </div>
+       </header>
+    );
 };
 
 export default Header;
