8ace6780a8f63521d5bbdc31f178a3f41938ee61
alex-shcherbakov <alex.shcherbakov.2@gmail.com>
2023-02-14 22:33:41 +0300

    added picture
    not valid url

---
+++ src/Widget/Picture/Picture.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './Picture.module.scss';
+
+const Picture = () => {
+    return (
+        <div className={styles.present}></div>
+    );
+}
+export default Picture;
