002ef712be5e2877e88c533424283c4403d78ad0
illyach <defusejoin@gmail.com>
2023-02-20 15:13:16 +0200

    'fix position and text font'

--- src/Components/UI/DetailsTitle/DetailsTitle.js
+++ src/Components/UI/DetailsTitle/DetailsTitle.js
@@ -2,7 +2,7 @@ import React from 'react';
 import s from './DetailsTitle.module.css'
 function DetailsTitle(props) {
     return (
-        <div className="all">
+        <div className={s.all}>
 
            <div className={s.image}>
            </div>
