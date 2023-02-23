3b77e37c3dd103392f2c329e028436206c3d52c7
Pro100Vlad1047 <Vladislav-10012004@ukr.net>
2023-02-24 00:30:00 +0100

    Revert "background fix"
    This reverts commit 7f1484058103c948f1c374db6ffde1d623791091.

--- src/Components/UI/Card/Card.jsx
+++ src/Components/UI/Card/Card.jsx
@@ -2,8 +2,7 @@ import styles from "./Card.module.scss";
 //import WidowPhoto from "../../../assets/images/Black_widow.jpg";
 import Rate from "../Rate/Rate";
 
-const Card = ({ data }) => {
-    console.log(data);
+const Card = ({data}) => {
     return <div className={styles.card_block}>
         <img src={data.poster} alt="Card_photo" />
         <p>{data.title}</p>
